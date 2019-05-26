23448c3671caa9706d5b0f981462cd983caa1487
snihar2 <36260910+snihar2@users.noreply.github.com>
2019-05-26 16:45:04 +0200

    Update readuart_Arduin-BBB-Dash.py

--- BBB-code/readuart_Arduin-BBB-Dash.py
+++ BBB-code/readuart_Arduin-BBB-Dash.py
@@ -6,6 +6,7 @@ import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 
 
+# selecting the uart on which you want to communicate
 UART.setup("UART1")
 
 disp = serial.Serial(port = "/dev/ttyO1", baudrate = 9600)
@@ -17,6 +18,7 @@ while True:
 	client = mqtt.Client()
 
 	client.connect("localhost", 1883, 60)
+	# initializing lists for string separation
 	chars = []
 	charsSpeed = []
 	charsOutput = []
@@ -29,25 +31,29 @@ while True:
 		print "I'm reading\n"
 		line = disp.readline()
 		print line
+		# getting all data retrieved from uart connection in same list
 		for i in line:
 			chars.append(i)
 
 		size1 = len(chars)
 		i = 0
 		while i < size1:
+			# getting speed data
 			if chars[i] == 'S':
 				case1 = 1
 				i = i + 1
 				while chars[i] != 'O':
 					charsSpeed.append(chars[i])
 					i = i + 1
-
+					
+			# getting output data
 			if chars[i] == 'O':
 				i = i + 1
 				while chars[i] != '\r':
 					charsOutput.append(chars[i])
 					i = i + 1
-
+			
+			# getting confirmation data
 			if chars[i] == "C":
 				case2 = 1
 				i = i + 1
@@ -55,7 +61,8 @@ while True:
 					charsConf.append(chars[i])
 					i = i + 1
 			i = size1
-
+		
+		# publish the different datasets on the right topics through MQTT
 		if case1 == 1:
 			valueSpeed = int("".join(map(str, charsSpeed)))
 			valueOutput = int("".join(map(str, charsOutput)))
